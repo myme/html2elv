ebc7079860fb77f170eb490a369d49990cd30c7d
Martin Øinæs Myrseth <myrseth@gmail.com>
2013-08-15 01:17:05 +0200

    Formatting / whitespace

--- bin/html2elv.js
+++ bin/html2elv.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var argv = require('optimist').argv;
 var Parser = require('../lib/parser').Parser;
 
-
 if (argv._.length !== 1) {
   console.log('Usage: html2elv <filename>');
   process.exit(1);
