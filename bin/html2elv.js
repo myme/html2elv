a85356e68db44f36b09220a6194539c5c69ed6b7
Martin Øinæs Myrseth <myrseth@gmail.com>
2013-08-15 10:19:50 +0200

    Read either from `stdio` or `file`

--- bin/html2elv.js
+++ bin/html2elv.js
@@ -1,14 +1,32 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var argv = require('optimist').argv;
+var optimist = require('optimist')
+  .usage('Usage: $0 [file]')
+  .describe('help', 'Display options')
+  .alias('help', 'h');
+var argv = optimist.argv;
 var Parser = require('../lib/parser').Parser;
 
-if (argv._.length !== 1) {
-  console.log('Usage: html2elv <filename>');
-  process.exit(1);
+if (argv.help) {
+  optimist.showHelp();
+  process.exit(0);
 }
 
-var filename = argv._[0];
-var data = fs.readFileSync(filename, 'utf8');
-console.log(new Parser().parse(data).toJavaScript());
+var stream, data = '';
+if (!argv._.length) {
+  stream = process.stdin;
+  stream.resume();
+} else {
+  stream = fs.createReadStream(argv._[0]);
+}
+
+stream.setEncoding('utf8');
+
+stream.on('data', function (chunk) {
+  data += chunk;
+});
+
+stream.on('end', function () {
+  console.log(new Parser().parse(data).toJavaScript());
+});
